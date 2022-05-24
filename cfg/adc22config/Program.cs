765724a82728a60b649e30f620ecd71c502c11cc
Nina <npollak@spectologic.com>
2022-05-24 17:34:32 +0200

    Configuration with Azure EventGrid and Servicebus

--- cfg/adc22config/Program.cs
+++ cfg/adc22config/Program.cs
@@ -1,6 +1,9 @@
 using adc22config.Models;
+using adc22config.Services;
 using Azure.Identity;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+IConfigurationRefresher? _refresher = null;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,18 +14,21 @@ builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
     {
         var connection = settings.GetConnectionString("AppConfig");
         config.AddAzureAppConfiguration(options =>
+        {
             options
                 .Connect(connection)
                 .ConfigureRefresh(refresh =>
                 {
-                    refresh.Register("TestApp:Settings:Sentinel",
-                        /* hostingContext.HostingEnvironment.EnvironmentName, */
-                        true)
-                        .SetCacheExpiration(TimeSpan.FromSeconds(30));
+                    refresh
+                        .Register("TestApp:Settings:Sentinel",
+                            /* hostingContext.HostingEnvironment.EnvironmentName, */
+                            true)
+                        .SetCacheExpiration(TimeSpan.FromDays(30)); // Important: Reduce poll frequency
                 })
                 .Select(KeyFilter.Any, LabelFilter.Null)
-                .Select(KeyFilter.Any, hostingContext.HostingEnvironment.EnvironmentName)
-        );
+                .Select(KeyFilter.Any, hostingContext.HostingEnvironment.EnvironmentName);
+            _refresher = options.GetRefresher();
+        });
     }
     else
     {
@@ -52,6 +58,9 @@ builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
 builder.Services.AddControllersWithViews();
 builder.Services.AddAzureAppConfiguration();
+builder.Services.AddHostedService<ConfigurationUpdateService>();
+builder.Services.AddSingleton<IConfigurationUpdater, ConfigurationUpdater>();
+if (_refresher != null) builder.Services.AddSingleton<IConfigurationRefresher>(_refresher);
 
 var app = builder.Build();
 
---
+++ cfg/adc22config/Services/ConfigurationUpdater.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System.Text.Json;
+
+namespace adc22config.Services;
+public class ConfigurationUpdater : IConfigurationUpdater
+{
+    private readonly ILogger<ConfigurationUpdater> _logger;
+    private Task _workerTask;
+    private readonly CancellationTokenSource cts = new CancellationTokenSource();
+    private readonly IConfigurationRefresher _refresher;
+
+    public ConfigurationUpdater(ILogger<ConfigurationUpdater> logger,
+            IConfigurationRefresher refresher)
+    {
+        _logger = logger;
+        _refresher = refresher;
+    }
+
+    public async Task StartMonitoring(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Start monitoring,... ");
+        _workerTask = Task.Run(async () => await Monitor(cts.Token));
+    }
+    public async Task StopMonitoring(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Stop monitoring,... ");
+        cts.Cancel();
+        return;
+    }
+    private async Task Monitor(CancellationToken cancellationToken)
+    {
+        var client = new ServiceBusClient("adc22config.servicebus.windows.net", new DefaultAzureCredential());
+        var receiver = client.CreateReceiver("cfgtopic", "cfgSubscription");
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var receivedMessage = await receiver.ReceiveMessageAsync();
+            if (receivedMessage != null)
+            {
+                try
+                {
+                    var messageText = receivedMessage.Body.ToString();
+                    var rootData = JsonDocument.Parse(messageText).RootElement;
+                    var messageData = rootData.GetProperty("data");
+                    var key = messageData.GetProperty("key").GetString();
+                    var syncToken = messageData.GetProperty("syncToken").GetString();
+                    var eventType = rootData.GetProperty("eventType").GetString();
+                    var subject = rootData.GetProperty("subject").GetString() ?? "https://nowhere.com";
+                    _logger.LogInformation($"Event received for Key = {key}");
+                    _refresher.ProcessPushNotification(
+                        new PushNotification()
+                        {
+                            SyncToken = syncToken,
+                            EventType = eventType,
+                            ResourceUri = new Uri(subject)
+                        }, new TimeSpan(0, 0, 0));
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Failed to process message!");
+                }
+            }
+            await Task.Delay(1000, cancellationToken);
+        }
+    }
+}
